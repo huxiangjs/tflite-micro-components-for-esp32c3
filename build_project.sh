d8fad9e65aae10247b439a16c3097f8dfebdc63e
Hoozz <huxiangjs@foxmail.com>
2024-03-27 00:08:24 +0800

    Add CMakeLists.txt file and re-build
    Signed-off-by: Hoozz <huxiangjs@foxmail.com>

--- build_project.sh
+++ build_project.sh
@@ -3,6 +3,8 @@
 echo "CROSS_TOOLS_PATH:" $1
 echo "job:" $2
 
+rm -rf project/gen
+
 cd project/
 make $2 COMMON_FLAGS="-DTF_LITE_STATIC_MEMORY -fno-unwind-tables -ffunction-sections -fdata-sections -fmessage-length=0 -march=rv32imc -Ofast" CXX=$1riscv32-esp-elf-g++ CC=$1riscv32-esp-elf-gcc AR=$1riscv32-esp-elf-ar
 
